9eea7da23617a06724a26d77c5f90fa750ac29fd
RostislavBartenev <rostislav.bartenev@gmail.com>
2024-09-12 14:35:07 +0300

    Integrate Mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {MantineProvider} from "@mantine/core";
+
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/tiptap/styles.css";
+
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MantineProvider>
+        <App />
+    </MantineProvider>
 )
