a55015e0dd0b92a55cce83d07ca7d9b83d052eb6
RostislavBartenev <rostislav.bartenev@gmail.com>
2024-09-12 15:09:00 +0300

    Fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import viteLogo from "/vite.svg";
-
+import { useState } from "react";
 
 import "./App.css";
 import reactLogo from "./assets/react.svg";
-import { useState } from "react";
+import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
