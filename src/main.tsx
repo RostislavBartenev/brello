4cacf476bc1e0d1bbff2d7348a75cee14b2847a6
RostislavBartenev <rostislav.bartenev@gmail.com>
2024-11-03 14:44:55 +0300

    feat: improve dnd interaction

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,8 @@ import { createRoot } from "react-dom/client";
 
 import { MantineProvider } from "@mantine/core";
 
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
-import "@mantine/dropzone/styles.css";
-import "@mantine/notifications/styles.css";
-import "@mantine/spotlight/styles.css";
-import "@mantine/tiptap/styles.css";
-
 import { Application } from "./application";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <MantineProvider>
